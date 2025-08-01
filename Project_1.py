a59a885cbfb100f2e234c824c47e18ef84744aa7
ledagale27 <ledagale@gmail.com>
2025-08-01 21:39:21 +0100

    Final commit

--- Project_1.py
+++ Project_1.py
@@ -23,4 +23,34 @@ print(players)
 max_score = 50
 player_scores = [0 for _ in range(players)]
 #player_scores = [0]*players
-print(player_scores)
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx +1, "turn has just started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)?")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break 
+            else:
+                current_score += value
+                print("You rolled a:", value)
+
+            print("Your score is:", current_score)
+
+        player_scores[player_idx] += current_score
+        print("Your total score is:", player_scores[player_idx])
+    
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, 
+      "is the winner with a score of:", max_score)
+
+      
